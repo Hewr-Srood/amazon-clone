595a4c6db7cea1f3aa84d4f065c6f3126f3c98f8
Hewr <hewr_s@outlook.com>
2021-07-21 15:34:51 +0300

    typscript template

---
+++ src/screens/HomeScreen/index.tsx
@@ -0,0 +1,13 @@
+import React, {FC} from 'react';
+import {View, StyleSheet} from 'react-native';
+
+const HomeScreen: FC = () => {
+  return (
+    <>
+      <View style={styles.container}></View>
+    </>
+  );
+};
+
+export default HomeScreen;
+const styles = StyleSheet.create({});
